76050a6b3a33df3a79d9cb5391245753b97e4c58
Cayo Legal <cayolegal@hotmail.com>
2024-06-23 19:23:55 -0400

    feat(config): add some project base config

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const print = (payload: unknown) => {
+  return console.log(payload);
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts"; // definition types plugin
 import { resolve } from "path";
 
 const config = defineConfig({
@@ -7,10 +8,12 @@ const config = defineConfig({
       entry: resolve(__dirname, "src/index.ts"),
       name: "utilities-lib",
     },
+    sourcemap: true,
+    target: "ESNext",
   },
-
+  plugins: [
+    dts() 
+  ],
 });
 
 export default config;
-
-
