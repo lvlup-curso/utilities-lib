cdbc61bdb192852921c26606fac9792483665bb7
Cayo Legal <cayolegal@hotmail.com>
2024-07-03 23:22:26 -0400

    fix(pr_issues): fix pr issues

--- src/tests/strings/capitalize.test.ts
+++ src/tests/strings/capitalize.test.ts
@@ -9,4 +9,10 @@ describe("Capitalize tests", () => {
   it("Should capitalize mundo", () => {
     expect(capitalize("mundo")).toBe("Mundo");
   });
+
+  it("Should return same value if is not string", () => {
+    const dummyValue = {};
+    const testValue = dummyValue as string;
+    expect(capitalize(testValue)).toStrictEqual({});
+  });
 });
